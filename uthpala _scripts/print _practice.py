5c83ee8de25e4ba718973bcb639a1b75d39c58d9
Durga Prasad <dprasad.tamirisa@gmail.com>
2018-04-20 23:34:31 +0200

    uthpala"s first commit

---
+++ uthpala _scripts/print _practice.py
@@ -0,0 +1,4 @@
+#This comment will be ignored by the compiler while executing the program.
+#whatever i want
+#Typically used for giving better comments about the code. 
+print("sritanvi  "+"durga "+"uthpala")
